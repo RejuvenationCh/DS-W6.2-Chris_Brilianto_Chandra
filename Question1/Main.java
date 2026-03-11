d1ca8b6801d01bb6722863f1c55d2b1e9785e53b
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-11 22:18:20 +0800

    Added class for letter

--- Question1/Main.java
+++ Question1/Main.java
@@ -1,4 +1,20 @@
 package Question1;
+class Letter {
+    String name;
+    int duration;
+    int priority;
+    int remainingTime;
+    String status; // "pending", "queued", "sent"
+
+    public Letter(String name, int duration, int priority) {
+        this.name = name;
+        this.duration = duration;
+        this.priority = priority;
+        this.remainingTime = duration;
+        this.status = "pending";
+    }
+}
 public class Main {
+
     
 }

5da50d1dc0da77916977069a777b2a44036d65ca
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-11 22:37:40 +0800

    Added some queuing logic

--- Question1/Main.java
+++ Question1/Main.java
@@ -7,13 +7,16 @@ class Letter {
     public int priority;
     public int remainingTime;
     public String status; 
+    public int arrivalOrder;
 
-    public Letter(String name, int duration, int priority) {
+    public Letter(String name, int duration, int priority, int arrivalOrder) {
         this.name = name;
         this.duration = duration;
         this.priority = priority;
         this.remainingTime = duration;
         this.status = "Pending";
+        this.arrivalOrder = arrivalOrder;
+
     }
 
 public static void main(String[] args){
@@ -29,5 +32,33 @@ public static void main(String[] args){
     Queue<Letter> processingQueue = new PriorityQueue<>();
     Queue<Letter> sentSQueue = new PriorityQueue<>();
 
-}}
+    for (int i = 0; i < userInput; i++) {
+        pendingQueue.add(new Letter(sc.next(), sc.nextInt(), sc.nextInt(), i));
+    }
+    int currentTime = 0;
+    
+    while (!pendingQueue.isEmpty()) {
+            Letter l = pendingQueue.poll();
+            l.status = "Queued";
+            processingQueue.add(l);
+        }
+        // Credit to gemini for Array Storage
+    while (!processingQueue.isEmpty()) {
+            currentTime++;
+            List<Letter> tempStorage = new ArrayList<>();
+            boolean changed = false;
+            for (Letter l : tempStorage) {
+                if (l.remainingTime <= 0) {
+                    l.status = "Sent";
+                    sentSQueue.add(l);
+                    changed = true;
+                } else {
+                    processingQueue.add(l);
+                }
+            }
+
+        
+    }
+}
+}
 

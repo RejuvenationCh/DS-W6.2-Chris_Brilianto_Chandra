129b83cfc120f77bbb29a50416fb9ebe1cf161e2
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-11 22:33:12 +0800

    Added Queues

--- Question1/Main.java
+++ Question1/Main.java
@@ -1,20 +1,33 @@
 package Question1;
+import java.util.*;
+
 class Letter {
-    String name;
-    int duration;
-    int priority;
-    int remainingTime;
-    String status; // "pending", "queued", "sent"
+    public String name;
+    public int duration;
+    public int priority;
+    public int remainingTime;
+    public String status; 
 
     public Letter(String name, int duration, int priority) {
         this.name = name;
         this.duration = duration;
         this.priority = priority;
         this.remainingTime = duration;
-        this.status = "pending";
+        this.status = "Pending";
     }
-}
-public class Main {
 
-    
-}
+public static void main(String[] args){
+    int duration;
+    int priority;
+    int remainingTime;
+    int finishTime;
+    int arrivalOrder;
+
+    Scanner sc = new Scanner(System.in);
+    int userInput = sc.nextInt();
+    Queue<Letter> pendingQueue = new PriorityQueue<>();
+    Queue<Letter> processingQueue = new PriorityQueue<>();
+    Queue<Letter> sentSQueue = new PriorityQueue<>();
+
+}}
+
